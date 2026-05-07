58288dc6b0b7b2e2b867dd2e54540f21d14d947a
sara-tavakoli <sara.tavakoligolpaygani@students.mq.edu.au>
2026-05-07 21:06:58 +1000

    add TutorService with real-time Firestore stream

---
+++ tutor_app/lib/services/tutor_service.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/tutor_model.dart';
+
+class TutorService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  // Real-time stream of all tutors
+  Stream<List<TutorModel>> getTutors() {
+    return _db.collection('tutors').snapshots().map((snap) =>
+        snap.docs.map((d) => TutorModel.fromMap(d.data(), d.id)).toList());
+  }
+
+  Future<TutorModel?> getTutorById(String uid) async {
+    final doc = await _db.collection('tutors').doc(uid).get();
+    if (!doc.exists) return null;
+    return TutorModel.fromMap(doc.data()!, doc.id);
+  }
+
+  Future<void> updateTutor(TutorModel tutor) async {
+    await _db.collection('tutors').doc(tutor.uid).update(tutor.toMap());
+  }
+
+  Future<void> deleteTutor(String uid) async {
+    await _db.collection('tutors').doc(uid).delete();
+  }
+}
