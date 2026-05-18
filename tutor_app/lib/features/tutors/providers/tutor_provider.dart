f4f85d12989a146fff6f98fa78192ce014991056
sara-tavakoli <sara.tavakoligolpaygani@students.mq.edu.au>
2026-05-18 14:15:24 +1000

    Add sort options to tutor feed (recommended, top rated, price)

--- tutor_app/lib/features/tutors/providers/tutor_provider.dart
+++ tutor_app/lib/features/tutors/providers/tutor_provider.dart
@@ -2,21 +2,36 @@ import 'package:flutter/foundation.dart';
 import '../../../models/tutor_model.dart';
 import '../../../services/tutor_service.dart';
 
+enum TutorSort { recommended, ratingDesc, priceLow, priceHigh }
+
+extension TutorSortLabel on TutorSort {
+  String get label {
+    switch (this) {
+      case TutorSort.recommended: return 'Recommended';
+      case TutorSort.ratingDesc:  return 'Top Rated';
+      case TutorSort.priceLow:    return 'Price: Low';
+      case TutorSort.priceHigh:   return 'Price: High';
+    }
+  }
+}
+
 class TutorProvider extends ChangeNotifier {
   final TutorService _service = TutorService();
 
-  List<TutorModel> _allTutors      = [];
-  String           _searchQuery    = '';
+  List<TutorModel> _allTutors       = [];
+  String           _searchQuery     = '';
   String           _selectedSubject = 'All';
-  bool             _isLoading      = true;
+  TutorSort        _sort            = TutorSort.recommended;
+  bool             _isLoading       = true;
 
   List<TutorModel> get allTutors       => _allTutors;
   String           get searchQuery     => _searchQuery;
   String           get selectedSubject => _selectedSubject;
+  TutorSort        get sort            => _sort;
   bool             get isLoading       => _isLoading;
 
   List<TutorModel> get filteredTutors {
-    return _allTutors.where((t) {
+    final filtered = _allTutors.where((t) {
       final matchesSearch = _searchQuery.isEmpty ||
           t.name.toLowerCase().contains(_searchQuery.toLowerCase()) ||
           t.subjects.any((s) =>
@@ -25,6 +40,22 @@ class TutorProvider extends ChangeNotifier {
           t.subjects.contains(_selectedSubject);
       return matchesSearch && matchesSubject;
     }).toList();
+
+    switch (_sort) {
+      case TutorSort.ratingDesc:
+        filtered.sort((a, b) => b.rating.compareTo(a.rating));
+      case TutorSort.priceLow:
+        filtered.sort((a, b) => a.hourlyRate.compareTo(b.hourlyRate));
+      case TutorSort.priceHigh:
+        filtered.sort((a, b) => b.hourlyRate.compareTo(a.hourlyRate));
+      case TutorSort.recommended:
+        filtered.sort((a, b) {
+          final score = (b.rating * b.reviewCount)
+              .compareTo(a.rating * a.reviewCount);
+          return score;
+        });
+    }
+    return filtered;
   }
 
   List<String> get allSubjects {
@@ -57,9 +88,15 @@ class TutorProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  void setSort(TutorSort sort) {
+    _sort = sort;
+    notifyListeners();
+  }
+
   void clearFilters() {
     _searchQuery     = '';
     _selectedSubject = 'All';
+    _sort            = TutorSort.recommended;
     notifyListeners();
   }
 }
